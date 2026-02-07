045ed6fdd7bc92f2bc080f75463bd5f0004d327e
Cheese Assistant <maqachimila43102@gmail.com>
2026-02-07 12:22:11 +0000

    Deploy: Successfully built Cheese's Nexus with live content

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,5 @@
+
+import __ASTRO_IMAGE_IMPORT_Z1Yq4sn from "../../assets/blog-placeholder-about.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md";
+import __ASTRO_IMAGE_IMPORT_MGbyL from "../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
+export default new Map([["../../assets/blog-placeholder-about.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md", __ASTRO_IMAGE_IMPORT_Z1Yq4sn], ["../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_MGbyL]]);
+		
