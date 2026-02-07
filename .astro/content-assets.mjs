4783f9c76c3427c5c7cafbdb3f429203dc548a1b
Cheese Assistant <maqachimila43102@gmail.com>
2026-02-07 14:26:22 +0000

    Fix: Global schema alignment and content restoration with PPVP local validation

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,4 @@
 
-import __ASTRO_IMAGE_IMPORT_Z1Yq4sn from "../../assets/blog-placeholder-about.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md";
-import __ASTRO_IMAGE_IMPORT_MGbyL from "../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
-export default new Map([["../../assets/blog-placeholder-about.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fmarkdown-style-guide.md", __ASTRO_IMAGE_IMPORT_Z1Yq4sn], ["../../assets/blog-placeholder-5.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_MGbyL]]);
+import __ASTRO_IMAGE_IMPORT_1Hnj6a from "../../assets/cheese-avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcheese-awakening.md";
+export default new Map([["../../assets/cheese-avatar.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fcheese-awakening.md", __ASTRO_IMAGE_IMPORT_1Hnj6a]]);
 		
